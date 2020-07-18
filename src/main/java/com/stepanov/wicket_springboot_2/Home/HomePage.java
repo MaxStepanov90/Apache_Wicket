de55acd9a7007410cf3eccba236f98acc5eb00b8
Max Stepanov <mu5cool@yandex.ru>
2020-07-18 20:51:02 +0300

    добавил webMarkContainer

--- src/main/java/com/stepanov/wicket_springboot_2/Home/HomePage.java
+++ src/main/java/com/stepanov/wicket_springboot_2/Home/HomePage.java
@@ -3,6 +3,7 @@ package com.stepanov.wicket_springboot_2.Home;
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
@@ -41,5 +42,15 @@ public class HomePage extends WebPage {
         form.add(new Button("submit"));
         add(form);
 
+        final int messages = 1;
+        WebMarkupContainer webMarkupContainer = new WebMarkupContainer("messages"){
+            @Override
+            protected void onConfigure() {
+                super.onConfigure();
+                setVisible(messages > 0);
+            }
+        };
+        webMarkupContainer.add(new Label("amountOfMessages", messages));
+        add(webMarkupContainer);
     }
 }
