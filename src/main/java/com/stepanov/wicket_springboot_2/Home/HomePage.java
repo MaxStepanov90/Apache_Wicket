cc40dc974e9a921aa29d6a21041c320dbfb1d1d8
Max Stepanov <mu5cool@yandex.ru>
2020-07-18 20:30:29 +0300

    Create form registration

--- src/main/java/com/stepanov/wicket_springboot_2/Home/HomePage.java
+++ src/main/java/com/stepanov/wicket_springboot_2/Home/HomePage.java
@@ -5,7 +5,11 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
 
 @WicketHomePage
 public class HomePage extends WebPage {
@@ -17,9 +21,25 @@ public class HomePage extends WebPage {
                 setResponsePage(FormPage.class);
             }
         });
-        Label label = new Label("label", "Test");
-        label.add(new AttributeModifier("style", "font-weight: bold"));
-        label.add(new AttributeAppender("style", "font-size: 20px; color: red"));
+
+        Label label = new Label("thanks","Спасибо за Регистрацию!");
+        label.add(new AttributeModifier("style","font-size: 25px; color: blue"));
+        label.setVisible(false);
         add(label);
+
+        Form form = new Form("form"){
+            @Override
+            protected void onSubmit() {
+                label.setVisible(true);
+                super.onSubmit();
+            }
+        };
+        form.add(new Label("name","Введите имя"));
+        form.add(new Label("surname","Введите фамилию"));
+        form.add(new TextField("nameText", Model.of("")));
+        form.add(new TextField("surnameText", Model.of("")));
+        form.add(new Button("submit"));
+        add(form);
+
     }
 }
